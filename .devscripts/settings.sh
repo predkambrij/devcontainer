6229f3ea737338ffa89c7abe7f584646158b0362
Alojzij Blatnik <lojze.blatnik@gmail.com>
2021-12-01 23:25:27 +0100

    upd compose project name

--- .devscripts/settings.sh
+++ .devscripts/settings.sh
@@ -1,4 +1,4 @@
-COMPOSE_PROJECT_NAME=$(basename $(realpath $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/) | tr '[:upper:]' '[:lower:]')
+COMPOSE_PROJECT_NAME=$(basename $(realpath $(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../) | tr '[:upper:]' '[:lower:]' | tr -d '.')
 NETWORK_NAME=devbox
 
 overrideScript=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/settingsOverride.sh
