a056b90742af170b3f4e5af65d69f6c6177fe30a
Alojzij Blatnik <alojzij.blatnik@gmail.com>
2026-02-04 15:02:43 +0100

    upd

--- .assets/ssh.sh
+++ .assets/ssh.sh
@@ -4,5 +4,7 @@ set -eEux -o pipefail
 shopt -s inherit_errexit
 
 dot_env=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../.env
-ssh $(grep -oP "(?<=ARG_UNAME=).*" "$dot_env")@$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $(docker compose ps -q)) "$@"
+username=$(grep -oP "(?<=ARG_UNAME=).*" "$dot_env")
+hostname=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $(docker compose ps -q))
+ssh "$username"@"$hostname" "$@"
 
--- .assets/sshp.sh
+++ .assets/sshp.sh
@@ -5,5 +5,6 @@ shopt -s inherit_errexit
 
 dot_env=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/../.env
 username=$(grep -oP "(?<=ARG_UNAME=).*" "$dot_env")
-sshpass -p "$username" ssh "$username"@$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $(docker compose ps -q)) "$@"
+hostname=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $(docker compose ps -q))
+sshpass -p "$username" ssh "$username"@"$hostname" "$@"
 
